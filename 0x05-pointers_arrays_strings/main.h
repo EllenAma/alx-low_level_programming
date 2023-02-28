4ea49857902f1fcf91f08db68daf13f2c7643108
EllenAma <ellenamankwah0@gmail.com>
2023-02-28 02:48:53 -0800

    Arrays are not pointers

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,6 @@
+#ifndef main_h
+#define main_h
+
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -8,3 +11,5 @@ void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
+
+#endif
